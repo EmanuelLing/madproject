71d86d91b4fd04db8dbe9ea93642dab9e664b385
ling <linghaoen@gmail.com>
2025-01-13 15:25:33 +0800

    initial commit

---
+++ app/src/main/java/com/example/madproject/ui/orderhistory/OrderHistoryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.madproject.ui.orderhistory;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class OrderHistoryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public OrderHistoryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is order history fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
